4a10caf2b54d92ff48f239375c30e1ac8242f022
Noor Ul Amin <noorawan444@gmail.com>
2023-10-28 21:17:59 +0500

    Wrap login & signup public route

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -6,21 +6,24 @@ import PageNotFound from './pages/page_not_found';
 import Home from './pages/home';
 import PrivateRoute from './components/protected_route';
 import Toast from './components/common/toast';
+import PublicRoute from './components/public_route';
 
 export function App() {
   return (
     <>
       <Toast />
       <Routes>
-        <Route element={<RootLayout />}>
-          <Route path="*" element={<PrivateRoute />}>
-            <Route index element={<Home />} />
-          </Route>
+        <Route element={<PublicRoute />}>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          {/* 👇️ only match this when no other routes match */}
-          <Route path="*" element={<PageNotFound />} />
         </Route>
+        <Route element={<RootLayout />}>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Route>
+        {/* 👇️ only match this when no other routes match */}
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
