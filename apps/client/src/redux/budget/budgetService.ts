7353fce9fb217eb29b809d772df9c2771009b805
Noor Ul Amin <noorawan444@gmail.com>
2023-10-29 11:21:58 +0500

    Add getPaginated budget entries RTQ api

--- apps/client/src/redux/budget/budgetService.ts
+++ apps/client/src/redux/budget/budgetService.ts
@@ -1,17 +1,38 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
-import { AddExpense, Expense } from '../../types/budget';
+import { AddExpense, Expense, PaginatedExpense } from '../../types/budget';
 
+type BudgetLimit = {
+  budgetLimit: number;
+};
+
+type BudgetStats = {
+  totalSpent: number;
+  remainingBudget: number;
+};
+
+interface UserBudgetLimitResponse {
+  data: BudgetLimit;
+  success: boolean;
+}
+interface UserBudgetLimitRequest {
+  budgetLimit: number;
+}
+interface UserBudgetStatsResponse {
+  data: BudgetStats;
+  success: boolean;
+}
 interface UserBudgetEntryResponse {
   data: Expense;
   success: boolean;
 }
 type UserBudgetEntryRequest = AddExpense;
+type PaginatedBudgetEntryResponse = PaginatedExpense;
 
 const baseUrl = import.meta.env.VITE_API_URL + '/api/';
 
-export const budgetEntryApi = createApi({
-  reducerPath: 'api/budget-entry',
+export const budgetExpenseApi = createApi({
+  reducerPath: 'api/budget',
   baseQuery: fetchBaseQuery({
     baseUrl,
     prepareHeaders: (headers, { getState }) => {
@@ -23,8 +44,53 @@ export const budgetEntryApi = createApi({
       return headers;
     },
   }),
-  tagTypes: ['BudgetEntry'],
+  tagTypes: ['BudgetEntry', 'BudgetLimit', 'BudgeStats'],
+
   endpoints: (builder) => ({
+    getUserBudgetLimit: builder.query<UserBudgetLimitResponse, void>({
+      query: () => ({
+        url: 'user/budget-limit',
+        method: 'GET',
+      }),
+      providesTags: ['BudgetLimit'],
+    }),
+    getUserBudgetStats: builder.query<UserBudgetStatsResponse, void>({
+      query: () => ({
+        url: 'user/budget-stats',
+        method: 'GET',
+      }),
+      providesTags: ['BudgeStats'],
+    }),
+    editBudgetLimit: builder.mutation<
+      UserBudgetLimitResponse,
+      UserBudgetLimitRequest
+    >({
+      query: (budgetData) => ({
+        url: 'user/budget-limit',
+        method: 'PUT',
+        body: budgetData,
+      }),
+      invalidatesTags: ['BudgetLimit', 'BudgeStats'],
+    }),
+
+    getBudgetEntries: builder.query<
+      PaginatedBudgetEntryResponse,
+      number | void
+    >({
+      query: (page = 1) => `budget-entries?page=${page}`,
+      providesTags: (result, error, page) =>
+        result
+          ? [
+              // Provides a tag for each row in the current page,
+              // as well as the 'PARTIAL-ENTRY' tag.
+              ...result.docs.map(({ _id }) => ({
+                type: 'BudgetEntry' as const,
+                _id,
+              })),
+              { type: 'BudgetEntry', id: 'PARTIAL-ENTRY' },
+            ]
+          : [{ type: 'BudgetEntry', id: 'PARTIAL-ENTRY' }],
+    }),
     addBudgetEntry: builder.mutation<
       UserBudgetEntryResponse,
       UserBudgetEntryRequest
@@ -34,8 +100,15 @@ export const budgetEntryApi = createApi({
         method: 'POST',
         body: budgetData,
       }),
+      invalidatesTags: ['BudgetEntry', 'BudgetLimit', 'BudgeStats'],
     }),
   }),
 });
 
-export const { useAddBudgetEntryMutation } = budgetEntryApi;
+export const {
+  useGetUserBudgetLimitQuery,
+  useEditBudgetLimitMutation,
+  useGetUserBudgetStatsQuery,
+  useGetBudgetEntriesQuery,
+  useAddBudgetEntryMutation,
+} = budgetExpenseApi;
