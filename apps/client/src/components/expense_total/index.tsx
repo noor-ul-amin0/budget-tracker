29b6eb0f7af4bfac6387cd3901d1bf1d61e8c347
Noor Ul Amin <noorawan444@gmail.com>
2023-10-29 07:55:04 +0500

    Replace dummy totalSpent with actual value
    - Also show Progress/Spinner on data load

--- apps/client/src/components/expense_total/index.tsx
+++ apps/client/src/components/expense_total/index.tsx
@@ -1,14 +1,25 @@
-import { Paper } from '@mui/material';
+import { Box, Paper } from '@mui/material';
 import { formatAsCurrency } from '../../utils/currency';
+import { FC } from 'react';
+import Progress from '../common/progress';
 
-const ExpenseTotal = () => {
+const ExpenseTotal: FC<{ totalSpent: number; isLoading: boolean }> = ({
+  totalSpent,
+  isLoading,
+}) => {
   return (
     <Paper
       elevation={2}
       sx={{ padding: 2, lineHeight: '36px' }}
       className="alert-primary"
     >
-      <span>Spent so far: {formatAsCurrency(4000)}</span>
+      <Box sx={{ display: 'flex', justifyContent: 'center', height: '36px' }}>
+        {isLoading ? (
+          <Progress />
+        ) : (
+          <span>Spent so far: {formatAsCurrency(totalSpent)}</span>
+        )}
+      </Box>
     </Paper>
   );
 };
