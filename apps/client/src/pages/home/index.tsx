93183e9dacadfe9ff94d887d800abe5ff4c192f9
Noor Ul Amin <noorawan444@gmail.com>
2023-10-29 09:49:28 +0500

    Remove static expenses list (home page)
    - TBD will integrate budget entries/expanses api

--- apps/client/src/pages/home/index.tsx
+++ apps/client/src/pages/home/index.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import { Container, Typography, Grid, Paper, Pagination } from '@mui/material';
 import ExpenseList from '../../components/expense_list';
-import AddExpenseDialog from '../../components/add_expense_dialogue';
-import { Expense } from '../../types/expense';
+import AddExpense from '../../components/add_expense';
+import { AddExpense as AddExpenseType, Expense } from '../../types/budget';
 import BudgetExpensesHeader from '../../components/budget_expenses_header';
 import BudgetExpensesControls from '../../components/budget_expense_control';
+import { useAddBudgetEntryMutation } from '../../redux/budget/budgetService';
+import { showToast } from '../../redux/toast/toastSlice';
+import { ToastType } from '../../constants/toast';
+import { useAppDispatch } from '../../hooks/store';
 
 const ITEMS_PER_PAGE = 10; // Number of items per page
 
-const expenses = Array.from({ length: 100 })
-  .fill(null)
-  .map((_, i) => ({ id: ++i + '', name: 'Expense ' + i, cost: i }));
-
 const Home = () => {
+  const dispatch = useAppDispatch();
   const [page, setPage] = useState(1);
   const [{ isEditMode, editableExpense }, setExpenseEditState] = useState<{
     isEditMode: boolean;
@@ -20,12 +21,14 @@ const Home = () => {
   }>({
     isEditMode: false,
     editableExpense: {
-      id: '',
+      _id: '',
       name: '',
       cost: 0,
     },
   });
   const [showDialog, setShowDialog] = useState(false);
+
+  const [addBudgetEntry, { isLoading }] = useAddBudgetEntryMutation();
 
   const handleEditExpense = (expense_item: Expense) => {
     setExpenseEditState({ isEditMode: true, editableExpense: expense_item });
@@ -36,7 +39,7 @@ const Home = () => {
     if (isEditMode) {
       setExpenseEditState({
         isEditMode: false,
-        editableExpense: { id: '', name: '', cost: 0 },
+        editableExpense: { _id: '', name: '', cost: 0 },
       });
     }
     setShowDialog(false);
@@ -53,10 +56,31 @@ const Home = () => {
     setPage(value);
   };
 
-  const paginatedExpenses = expenses.slice(
-    (page - 1) * ITEMS_PER_PAGE,
-    page * ITEMS_PER_PAGE
-  );
+  const handleAddBudgetEntrySubmit = async (expense: AddExpenseType) => {
+    try {
+      await addBudgetEntry(expense).unwrap();
+      dispatch(
+        showToast({
+          type: ToastType.SUCCESS,
+          message: 'Expense added successfully',
+        })
+      );
+      setShowDialog(false);
+    } catch (error: any) {
+      let message = '';
+      if (error.data && error.data.message) {
+        message = error.data.message;
+      } else {
+        message = error.message;
+      }
+      dispatch(
+        showToast({
+          type: ToastType.ERROR,
+          message,
+        })
+      );
+    }
+  };
 
   return (
     <Container>
@@ -72,7 +96,7 @@ const Home = () => {
         <Grid item xs={12}>
           <Paper>
             <ExpenseList
-              expenses={paginatedExpenses}
+              expenses={[]}
               handleEditExpense={handleEditExpense}
               handleDeleteExpense={handleDeleteExpense}
             />
@@ -81,17 +105,19 @@ const Home = () => {
       </Grid>
       <Grid container justifyContent="center" mt={2}>
         <Pagination
-          count={Math.ceil(expenses.length / ITEMS_PER_PAGE)}
+          count={Math.ceil(ITEMS_PER_PAGE)}
           page={page}
           onChange={handlePageChange}
         />
       </Grid>
       {showDialog && (
-        <AddExpenseDialog
+        <AddExpense
           open={showDialog}
           isEditMode={isEditMode}
           initialData={editableExpense}
+          isLoading={isLoading}
           onClose={handleClose}
+          handleBudgetEntrySubmit={handleAddBudgetEntrySubmit}
         />
       )}
     </Container>
