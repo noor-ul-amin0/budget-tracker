17485673feb8f0c527de093e4bdaf2d5db78e11c
Noor Ul Amin <noorawan444@gmail.com>
2023-10-29 07:52:43 +0500

    Integrate edit budget limit mutation (RTQ)

--- apps/client/src/components/budget/index.tsx
+++ apps/client/src/components/budget/index.tsx
@@ -1,18 +1,54 @@
 import React, { useState } from 'react';
-import { Paper } from '@mui/material';
+import { Box, CircularProgress, Paper } from '@mui/material';
 import ViewBudget from '../view_budget';
 import EditBudget from '../edit_budget';
+import {
+  useEditBudgetLimitMutation,
+  useGetUserBudgetLimitQuery,
+} from '../../redux/user/userService';
+import { useAppDispatch } from '../../hooks/store';
+import { showToast } from '../../redux/toast/toastSlice';
+import { ToastType } from '../../constants/toast';
 
 const Budget = () => {
+  const dispatch = useAppDispatch();
+  const { data: budgetData, isLoading } = useGetUserBudgetLimitQuery();
+  const [editBudgetLimit, { isLoading: isMutationLoading }] =
+    useEditBudgetLimitMutation();
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
-  const handleSaveClick = (value) => {
-    setIsEditing(false);
+  const handleSaveClick = async (budgetLimit: number) => {
+    try {
+      await editBudgetLimit({ budgetLimit }).unwrap();
+      dispatch(
+        showToast({
+          type: ToastType.SUCCESS,
+          message: 'Budget limit updated successfully',
+        })
+      );
+    } catch (error: any) {
+      let message = '';
+      if (error.data && error.data.message) {
+        message = error.data.message;
+      } else {
+        message = error.message;
+      }
+
+      dispatch(
+        showToast({
+          type: ToastType.ERROR,
+          message,
+        })
+      );
+    } finally {
+      setIsEditing(false);
+    }
   };
+
   return (
     <Paper
       elevation={2}
@@ -23,10 +59,27 @@ const Budget = () => {
         justifyContent: 'space-between',
       }}
     >
-      {isEditing ? (
-        <EditBudget budget={45} handleSaveClick={handleSaveClick} />
+      {isLoading || isMutationLoading ? (
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            height: '36px',
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : isEditing ? (
+        <EditBudget
+          budget={budgetData?.data.budgetLimit || 0}
+          handleSaveClick={handleSaveClick}
+        />
       ) : (
-        <ViewBudget budget={43} handleEditClick={handleEditClick} />
+        <ViewBudget
+          budget={budgetData?.data.budgetLimit || 0}
+          handleEditClick={handleEditClick}
+        />
       )}
     </Paper>
   );
