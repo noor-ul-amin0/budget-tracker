4f9bcbc86e2c825d01ad2a05c4307cdea83a93a9
Noor Ul Amin <noorawan444@gmail.com>
2023-10-29 07:52:03 +0500

    Fetch budget stats in BudgetExpensesHeader
    - This api includes total spent and remaining budget.
    - Then the data is passed down to corresponding childrens.

--- apps/client/src/components/budget_expenses_header/index.tsx
+++ apps/client/src/components/budget_expenses_header/index.tsx
@@ -2,8 +2,10 @@ import { Grid, Paper } from '@mui/material';
 import Budget from '../budget';
 import RemainingBudget from '../remaining_budget/remaining_budget';
 import ExpenseTotal from '../expense_total';
+import { useGetUserBudgetStatsQuery } from '../../redux/user/userService';
 
 const BudgetExpensesHeader = () => {
+  const { data: budgetStats, isLoading } = useGetUserBudgetStatsQuery();
   return (
     <Grid container mt={3} spacing={2}>
       <Grid item xs={12} sm={4}>
@@ -13,12 +15,18 @@ const BudgetExpensesHeader = () => {
       </Grid>
       <Grid item xs={12} sm={4}>
         <Paper>
-          <RemainingBudget />
+          <RemainingBudget
+            remainingBudget={budgetStats?.data.remainingBudget || 0}
+            isLoading={isLoading}
+          />
         </Paper>
       </Grid>
       <Grid item xs={12} sm={4}>
         <Paper>
-          <ExpenseTotal />
+          <ExpenseTotal
+            totalSpent={budgetStats?.data.totalSpent || 0}
+            isLoading={isLoading}
+          />
         </Paper>
       </Grid>
     </Grid>
