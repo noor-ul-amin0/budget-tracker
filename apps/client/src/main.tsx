ff0bba52fe6ea484e4146e5303574aa6e8d897a3
Noor Ul Amin <noorawan444@gmail.com>
2023-10-30 11:50:57 +0500

    Replace dayjs adapter with date-fns (date picker)

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -5,7 +5,7 @@ import App from './app';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline, StyledEngineProvider, createTheme } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -24,7 +24,7 @@ root.render(
         <StyledEngineProvider injectFirst>
           <ThemeProvider theme={defaultTheme}>
             <CssBaseline />
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
               <PersistGate loading={null} persistor={persistor}>
                 <App />
               </PersistGate>
