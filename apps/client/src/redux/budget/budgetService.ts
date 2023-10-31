6eac71f59a825c91cfaaf40278cbb0088fb5da77
Noor Ul Amin <noorawan444@gmail.com>
2023-10-31 10:32:10 +0500

    Logout user on token expiry

--- apps/client/src/redux/budget/budgetService.ts
+++ apps/client/src/redux/budget/budgetService.ts
@@ -1,6 +1,15 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  createApi,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
 import { AddExpense, Expense, PaginatedExpense } from '../../types/budget';
+import { logout } from '../auth/authSlice';
+import { showToast } from '../toast/toastSlice';
+import { ToastType } from '../../constants/toast';
 
 type BudgetLimit = {
   budgetLimit: number;
@@ -32,19 +41,34 @@ type PaginatedBudgetEntryResponse = PaginatedExpense;
 
 const baseUrl = import.meta.env.VITE_API_URL + '/api/';
 
+const baseQuery = fetchBaseQuery({
+  baseUrl,
+  prepareHeaders: (headers, { getState }) => {
+    // if we have a token in the store, use that for authenticated requests
+    const token = (getState() as RootState).auth.token;
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+  if (result.error && result.error.status === 401) {
+    api.dispatch(showToast({ message: 'Please login', type: ToastType.ERROR }));
+    api.dispatch(logout());
+  }
+  return result;
+};
+
 export const budgetExpenseApi = createApi({
   reducerPath: 'api/budget',
-  baseQuery: fetchBaseQuery({
-    baseUrl,
-    prepareHeaders: (headers, { getState }) => {
-      // if we have a token in the store, use that for authenticated requests
-      const token = (getState() as RootState).auth.token;
-      if (token) {
-        headers.set('authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: baseQueryWithReauth,
   tagTypes: ['BudgetEntry', 'BudgetLimit', 'BudgeStats', 'BudgeTrends'],
 
   endpoints: (builder) => ({
