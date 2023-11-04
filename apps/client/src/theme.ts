ab9fc43b16947daf6cf11df03d69e53f888f935b
Noor Ul Amin <noorawan444@gmail.com>
2023-11-04 11:06:28 +0500

    Add custom theme file with default styles

---
+++ apps/client/src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1398E8',
+    },
+    background: {
+      default: '#F2F7FF',
+    },
+  },
+});
+
+export default theme;
