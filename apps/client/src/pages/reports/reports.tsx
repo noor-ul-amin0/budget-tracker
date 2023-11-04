8e2d7d04a90a66c7653d056d3e4012b06bb4d1c1
Noor Ul Amin <noorawan444@gmail.com>
2023-11-04 11:17:34 +0500

    Replace react chart with mui-chart lib

--- apps/client/src/pages/reports/reports.tsx
+++ apps/client/src/pages/reports/reports.tsx
@@ -1,22 +1,14 @@
+import * as React from 'react';
+import { BarPlot } from '@mui/x-charts/BarChart';
+import { LinePlot } from '@mui/x-charts/LineChart';
+import { ChartContainer } from '@mui/x-charts/ChartContainer';
+import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
+import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
+import { Alert, Box, Grid, Typography } from '@mui/material';
 import { useGetBudgetTrendsQuery } from '../../redux/budget/budgetService';
-import LineChart from '../../components/common/line_chart/line_chart';
-import { Box, Grid } from '@mui/material';
 import Progress from '../../components/common/progress/progress';
 
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Budget Trends (Reports)',
-    },
-  },
-};
-
-const Reports = () => {
+export default function Reports() {
   const { data, isLoading, isFetching } = useGetBudgetTrendsQuery();
 
   if (isLoading || isFetching) {
@@ -33,25 +25,48 @@ const Reports = () => {
   }
 
   return (
-    <Grid container height="90vh">
+    <Grid container mt={5}>
       <Grid
         display={'flex'}
         justifyContent="center"
         alignItems="center"
+        flexDirection={'column'}
+        gap={5}
         item
         xs={12}
       >
-        <Box
-          sx={{
-            width: { xs: '90%', md: '70%' },
-            margin: '0 auto',
-          }}
-        >
-          <LineChart chartOptions={options} chartData={data} />
+        <Box>
+          <Alert severity="warning">
+            <Typography>
+              <em>Important Note:</em> The red line on the chart indicates that
+              your expenses have exceeded your budget limit. Please manage your
+              expenses accordingly.
+            </Typography>
+          </Alert>
+        </Box>
+        <Box>
+          <ChartContainer
+            series={data?.series}
+            width={600}
+            height={450}
+            xAxis={data?.xAxis}
+            yAxis={data?.yAxis}
+          >
+            <BarPlot tooltip={{ trigger: 'axis' }} />
+            <LinePlot tooltip={{ trigger: 'item' }} />
+            <ChartsXAxis
+              label="Expenses"
+              position="bottom"
+              axisId={data?.xAxis[0]?.id}
+            />
+            <ChartsYAxis
+              label="Budget"
+              position="left"
+              axisId={data?.yAxis[0]?.id}
+            />
+          </ChartContainer>
         </Box>
       </Grid>
     </Grid>
   );
-};
-
-export default Reports;
+}
