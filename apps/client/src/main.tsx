c813f30bba7ca6d26583c6e0878fe1f95c078cac
Noor Ul Amin <noorawan444@gmail.com>
2023-11-04 11:06:46 +0500

    Include custom theme in main

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,26 +3,26 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app';
 import { BrowserRouter } from 'react-router-dom';
-import { CssBaseline, StyledEngineProvider, createTheme } from '@mui/material';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
+import theme from './theme';
+import '@fontsource/roboto';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const defaultTheme = createTheme();
 
 root.render(
   <StrictMode>
     <BrowserRouter>
       <ReduxProvider store={store}>
         <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={defaultTheme}>
+          <ThemeProvider theme={theme}>
             <CssBaseline />
             <LocalizationProvider dateAdapter={AdapterDateFns}>
               <PersistGate loading={null} persistor={persistor}>
