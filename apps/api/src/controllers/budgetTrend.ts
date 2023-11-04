1fe5f92425f9a5df87b1a3eb1cad214d37ffa3c1
Noor Ul Amin <noorawan444@gmail.com>
2023-11-04 11:04:44 +0500

    Update reports api response for mui chart

--- apps/api/src/controllers/budgetTrend.ts
+++ apps/api/src/controllers/budgetTrend.ts
@@ -63,27 +63,42 @@ export const getBudgetTrends = catchAsyncAwait(
     // Check if the total budget entries exceed the user's budget limit
     const user = req.user as any; // Modify this type according to your User model
 
-    // Prepare data for the chart (e.g., using a charting library like Chart.js)
+    // Prepare data for the chart
     const chartData = {
-      labels: ['Last Month', 'Last 6 Months', 'Last 12 Months'],
-      datasets: [
+      series: [
         {
-          label: 'Total Expenses',
+          type: 'bar',
+          stack: '',
+          yAxisKey: 'total',
           data: [
             lastMonthTotal[0]?.total || 0,
             last6MonthsTotal[0]?.total || 0,
             last12MonthsTotal[0]?.total || 0,
           ],
-          backgroundColor: 'rgba(75, 192, 192, 0.2', // Set your preferred chart colors
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
         },
         {
-          label: 'Budget Limit',
+          type: 'line',
+          yAxisKey: 'budgetLimit',
+          color: 'red',
           data: [user.budgetLimit, user.budgetLimit, user.budgetLimit],
-          backgroundColor: 'rgba(255, 99, 132, 0.2', // Set your preferred chart colors
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1,
+        },
+      ],
+      xAxis: [
+        {
+          id: 'expenses',
+          data: ['Last Month', 'Last 6 Months', 'Last 12 Months'],
+          scaleType: 'band',
+          valueFormatter: (value) => value.toString(),
+        },
+      ],
+      yAxis: [
+        {
+          id: 'total',
+          scaleType: 'linear',
+        },
+        {
+          id: 'budgetLimit',
+          scaleType: 'log',
         },
       ],
     };
