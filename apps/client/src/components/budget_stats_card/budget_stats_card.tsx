4d09b10b615541e73ef4292e4e803fa3565b16cd
Noor Ul Amin <noorawan444@gmail.com>
2023-11-04 11:10:57 +0500

    Add budget stats card component to show user budget

---
+++ apps/client/src/components/budget_stats_card/budget_stats_card.tsx
@@ -0,0 +1,124 @@
+import React, { FC, ReactNode, useEffect, useState } from 'react';
+import {
+  Typography,
+  CircularProgress,
+  Box,
+  Fab,
+  Input,
+  IconButton,
+} from '@mui/material';
+import { formatAsCurrency } from '../../utils/currency';
+import Card from '../common/card/card';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import styles from './budget_stats_card.module.scss';
+
+interface BudgetStatsCardProps {
+  isLoading: boolean;
+  title: string;
+  value: number;
+  action?: boolean;
+  error?: string | null;
+  icon?: ReactNode;
+  handleEditLimit?: (value: number) => void;
+}
+
+const BudgetStatsCard: FC<BudgetStatsCardProps> = ({
+  isLoading,
+  title,
+  value,
+  error,
+  icon,
+  action = false,
+  handleEditLimit,
+}) => {
+  const [editMode, setEditMode] = useState(false);
+  const [editedValue, setEditedValue] = useState(value);
+
+  useEffect(() => {
+    setEditedValue(value);
+  }, [value]);
+
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleSaveClick = () => {
+    setEditMode(false);
+    handleEditLimit?.(editedValue);
+  };
+
+  const handleCancelClick = () => {
+    setEditMode(false);
+    setEditedValue(value); // Reset the edited value to the original value.
+  };
+
+  return (
+    <Card>
+      <Box className={styles.budget_stats_container}>
+        {isLoading ? (
+          <Box className={styles.budget_stats_progress}>
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            {icon && (
+              <Fab color="primary" className={styles.icon}>
+                {icon}
+              </Fab>
+            )}
+            <Box>
+              <Typography
+                sx={{
+                  fontFamily: 'Roboto',
+                }}
+                className={styles.title}
+              >
+                {title}
+              </Typography>
+
+              {editMode ? (
+                <Box className={styles.action_box}>
+                  <Input
+                    type="number"
+                    value={editedValue}
+                    onChange={(e) => setEditedValue(Number(e.target.value))}
+                  />
+                  <IconButton onClick={handleSaveClick}>
+                    <CheckIcon />
+                  </IconButton>
+                  <IconButton onClick={handleCancelClick}>
+                    <CloseIcon />
+                  </IconButton>
+                </Box>
+              ) : (
+                <>
+                  <Typography className={styles.currency}>
+                    {formatAsCurrency(value)}
+                  </Typography>
+                  {error && (
+                    <Box>
+                      <Typography className={styles.error}>{error}</Typography>
+                    </Box>
+                  )}
+                </>
+              )}
+            </Box>
+            {action && (
+              <Box className={styles.edit_icon}>
+                {!editMode && (
+                  <IconButton onClick={handleEditClick}>
+                    <EditIcon color="primary" />
+                  </IconButton>
+                )}
+              </Box>
+            )}
+          </>
+        )}
+      </Box>
+    </Card>
+  );
+};
+
+export default BudgetStatsCard;
