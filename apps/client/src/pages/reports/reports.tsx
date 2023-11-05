6d4adbb5915007f4695946938361a08aa9907314
Noor Ul Amin <noorawan444@gmail.com>
2023-11-05 17:25:28 +0500

    Replace mui x-charts with recharts

--- apps/client/src/pages/reports/reports.tsx
+++ apps/client/src/pages/reports/reports.tsx
@@ -1,72 +1,108 @@
-import * as React from 'react';
-import { BarPlot } from '@mui/x-charts/BarChart';
-import { LinePlot } from '@mui/x-charts/LineChart';
-import { ChartContainer } from '@mui/x-charts/ChartContainer';
-import { ChartsXAxis } from '@mui/x-charts/ChartsXAxis';
-import { ChartsYAxis } from '@mui/x-charts/ChartsYAxis';
-import { Alert, Box, Grid, Typography } from '@mui/material';
+import React from 'react';
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  Line,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from 'recharts';
 import { useGetBudgetTrendsQuery } from '../../redux/budget/budgetService';
+import { Alert, Box, Typography } from '@mui/material';
 import Progress from '../../components/common/progress/progress';
+import styles from './reports.module.scss';
 
-export default function Reports() {
+const BudgetTrendsChart = () => {
   const { data, isLoading, isFetching } = useGetBudgetTrendsQuery();
 
   if (isLoading || isFetching) {
     return (
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        minHeight={'90vh'}
-      >
+      <Box className={styles.box_container} minHeight={'90vh'}>
         <Progress size={60} />
       </Box>
     );
   }
+  const chartData = [
+    {
+      name: 'Last Month',
+      'Budget Limit': data?.userBudgetLimit,
+      Expenses: data?.lastMonthTotal,
+    },
+    {
+      name: 'Last 6 Months',
+      'Budget Limit': data?.userBudgetLimit,
+      Expenses: data?.last6MonthsTotal,
+    },
+    {
+      name: 'Last 12 Months',
+      'Budget Limit': data?.userBudgetLimit,
+      Expenses: data?.last12MonthsTotal,
+    },
+  ];
 
   return (
-    <Grid container mt={5}>
-      <Grid
-        display={'flex'}
-        justifyContent="center"
-        alignItems="center"
-        flexDirection={'column'}
-        gap={5}
-        item
-        xs={12}
+    <div className={styles.chart_container}>
+      <Box
+        sx={{
+          width: {
+            xs: '90%',
+            sm: '80%',
+            md: '60%',
+          },
+        }}
+        className={styles.box_container}
       >
-        <Box>
-          <Alert severity="warning">
-            <Typography>
-              <em>Important Note:</em> The red line on the chart indicates that
-              your expenses have exceeded your budget limit. Please manage your
-              expenses accordingly.
-            </Typography>
-          </Alert>
-        </Box>
-        <Box>
-          <ChartContainer
-            series={data?.series}
-            width={600}
-            height={450}
-            xAxis={data?.xAxis}
-            yAxis={data?.yAxis}
+        <Alert severity="warning">
+          <Typography>
+            <em>Important Note:</em> The red line on the chart indicates that
+            your expenses have exceeded your budget limit. Please manage your
+            expenses accordingly.
+          </Typography>
+        </Alert>
+      </Box>
+      <Box
+        sx={{
+          width: {
+            xs: '100%',
+            sm: '80%',
+            md: '40%',
+          },
+          height: {
+            xs: 'auto',
+            sm: 'auto',
+            md: '400px',
+            lg: '450px',
+            xl: '500px',
+          },
+        }}
+      >
+        <ResponsiveContainer>
+          <ComposedChart
+            data={chartData}
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
           >
-            <BarPlot tooltip={{ trigger: 'axis' }} />
-            <LinePlot tooltip={{ trigger: 'item' }} />
-            <ChartsXAxis
-              label="Expenses"
-              position="bottom"
-              axisId={data?.xAxis[0]?.id}
+            <XAxis dataKey="name" scale="band" />
+            <YAxis />
+            <Tooltip />
+            <Legend
+              wrapperStyle={{ bottom: '1px' }}
+              iconSize={20}
+              iconType="circle"
             />
-            <ChartsYAxis
-              label="Budget"
-              position="left"
-              axisId={data?.yAxis[0]?.id}
-            />
-          </ChartContainer>
-        </Box>
-      </Grid>
-    </Grid>
+            <Bar dataKey="Expenses" barSize={20} fill="#413ea0" />
+            <Line type="monotone" dataKey="Budget Limit" stroke="#ff7300" />
+          </ComposedChart>
+        </ResponsiveContainer>
+      </Box>
+    </div>
   );
-}
+};
+
+export default BudgetTrendsChart;
